8dd4e77c64c67110545bb79e51c68ef646476a61
Shane Cavanaugh <shane@shanecav.net>
2019-10-18 19:58:54 -0400

    Create container for Rack app specs

--- spec/rack_spec_helper.rb
+++ spec/rack_spec_helper.rb
@@ -7,6 +7,35 @@ require 'rack/test'
 require 'social_web/rack'
 require_relative './helpers/rack_helper'
 
+class RackSpecContainer < SocialWeb::Rack::Container
+  extend Dry::Container::Mixin
+
+  namespace(:repositories) do
+    register(:keys) do
+      keys = Object.new
+      def keys.for_actor_iri(actor_iri)
+        keypair = OpenSSL::PKey::RSA.new(2048)
+        { private: keypair.to_pem, public: keypair.public_key.to_pem }
+      end
+      keys
+    end
+  end
+
+  namespace(:services) do
+    register(:dereference) do
+      dereference = Object.new
+      def dereference.call(iri)
+        object = OpenStruct.new
+        object.inbox = "#{iri}/inbox"
+        object
+      end
+      dereference
+    end
+  end
+end
+
+SocialWeb.config.container = RackSpecContainer
+
 RSpec.configure do |config|
   config.include ::RackHelper
   config.include ::Rack::Test::Methods, type: :request
