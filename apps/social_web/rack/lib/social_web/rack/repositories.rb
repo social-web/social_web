e6dc0e12fa0c1325594f2961e0eee5a48d3e11b0
Shane Cavanaugh <shane@shanecav.net>
2019-11-19 22:08:49 -0500

    Remove obsolete service and repo

--- apps/social_web/rack/lib/social_web/rack/repositories.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require 'social_web/rack/repositories/activities'
-require 'social_web/rack/repositories/actors'
-require 'social_web/rack/repositories/keys'
--- apps/social_web/rack/lib/social_web/rack/services.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require 'social_web/rack/services/delivery'
-require 'social_web/rack/services/dereference'
