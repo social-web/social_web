b729dc60439a6cc813a617d287004b1334cd8040
Shane Cavanaugh <shane@shanecav.net>
2020-02-09 15:24:55 -0700

    Clear up keys repo API and docs

--- app/social_web/repositories/keys.rb
+++ app/social_web/repositories/keys.rb
@@ -3,20 +3,24 @@
 module SocialWeb
   module Repositories
     class Keys
-      def for_actor(actor)
-        keys = SocialWeb[:db][:social_web_keys].first(object_iri: actor[:id])
+      # @param obj [ActivityStreams] the object to the keys for
+      # @return [Hash]
+      def get_keys_for(obj)
+        keys = SocialWeb[:db][:social_web_keys].first(object_iri: obj[:id])
         timestamp = keys[:updated_at].iso8601
         {
-          key_id: "#{actor[:id]}/#key-#{timestamp}",
+          key_id: "#{obj[:id]}/#key-#{timestamp}",
           private: keys[:private],
           public: keys[:public]
         }
       end
 
-      def generate_for_actor(actor)
+      # @param obj [ActivityStreams] the object to the keys for
+      # @return [Hash]
+      def generate_for_actor(obj)
         keypair = OpenSSL::PKey::RSA.new(2048)
         SocialWeb[:db][:social_web_keys].insert(
-          object_iri: actor[:id],
+          object_iri: obj[:id],
           private: keypair.to_pem,
           public: keypair.public_key.to_pem,
           created_at: Time.now.utc,
