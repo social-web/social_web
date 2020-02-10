94de7f046cccd7417e3a82998206aa1481e340a1
Shane Cavanaugh <shane@shanecav.net>
2020-02-09 21:24:00 -0700

    Show list of items from collection for actor

--- app/social_web/routes.rb
+++ app/social_web/routes.rb
@@ -14,31 +14,49 @@ module SocialWeb
     plugin :halt
     plugin :json
     plugin :middleware
-    plugin :render,
-      engine: 'html.slim',
-      views: File.join(__dir__, 'views')
-    plugin :type_routing,
-      types: { activity_json: 'application/activity+json' }
-    plugin :default_headers,
-      'Content-Type' => 'text/html; charset=utf-8'
+    plugin :type_routing, types: { activity_json: 'application/activity+json' }
+    plugin :default_headers, 'Content-Type' => 'text/html; charset=utf-8'
     plugin :common_logger, SocialWeb[:config].loggers
 
     route do |r|
+      iri = r.url
+
       r.activity_json do
         r.get do
-          found = SocialWeb['repositories.objects'].get_by_iri(r.url)
+          found = SocialWeb['repositories.objects'].get_by_iri(iri)
           found.nil? ? r.halt(404) : found.to_json
         end
 
         r.post do
           activity_json = r.body.read
-          actor_iri = parse_actor_iri(r.url)
-          collection = parse_collection(r.url)
+          actor_iri = parse_actor_iri(iri)
+          collection = parse_collection(iri)
 
           SocialWeb.process(activity_json, actor_iri, collection)
 
           response.status = 201
           ''
+        end
+      end
+
+      r.on(COLLECTION_REGEX) do
+        r.get do
+          actor_iri = parse_actor_iri(iri)
+          actor = SocialWeb['repositories.objects'].get_by_iri(actor_iri)
+          collection = parse_collection(iri)
+
+          collection = SocialWeb['repositories.collections'].get_collection_for_actor(actor: actor, collection: collection)
+
+          response.headers['Content-Type'] = 'text/plain'
+
+          items_list = collection[:items].map { |item| item[:id] }.join("\n")
+
+          <<~TXT
+            INBOX for #{actor_iri}
+            #{collection[:items].count}
+
+            #{items_list}
+          TXT
         end
       end
     end
