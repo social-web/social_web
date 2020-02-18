88215c25727819a9a92065458045e42db7915b3b
Shane Cavanaugh <shane@shanecav.net>
2020-02-17 17:40:28 -0700

    Revert "Store activity before processing"
    This reverts commit 36d0d174ecac65e068d90d3f7c3f497698231a73.

--- lib/social_web/process.rb
+++ lib/social_web/process.rb
@@ -5,9 +5,9 @@ module SocialWeb
     activity = ActivityStreams.from_json(activity_json)
     actor = SocialWeb['repositories.objects'].get_by_iri(actor_iri)
 
-    SocialWeb['repositories.objects'].store(activity)
     SocialWeb['services.dereference'].for_actor(actor).call(activity)
     activity = SocialWeb['services.reconstitute'].call(activity)
+    SocialWeb['repositories.objects'].store(activity)
 
     SocialWeb["collections.#{collection}"].for_actor(actor).process(activity)
   end
