6fc36010675e6f2d0e9ff46b87b5c24806d5fac3
Shane Cavanaugh <shane@shanecav.net>
2020-02-26 07:51:16 -0700

    Add "hostname" configuration key

--- system/social_web/configuration.rb
+++ system/social_web/configuration.rb
@@ -5,6 +5,7 @@ module SocialWeb
 
   setting(:logger, SocialWeb[:logger])
   setting(:collections, %i[inbox outbox].freeze) { |collection| Array(collection).freeze }
+  setting(:hostname)
 
   # When traversing an ActivityStream's property tree, how deep should we go
   # Default: Float::INFINITY
